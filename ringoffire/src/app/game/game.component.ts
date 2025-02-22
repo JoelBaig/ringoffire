714ad2083a842db0e39d47989c7f3ae0388dc91e
JoelBaig <131587984+Joe482@users.noreply.github.com>
2025-02-22 23:06:47 +0100

    added more cards with ngFor

--- ringoffire/src/app/game/game.component.ts
+++ ringoffire/src/app/game/game.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
 export class GameComponent {
-
+  @Input() card = {
+    image: './assets/img/cards/card_cover.png',
+    name: 'Card Name'
+  }
 }
