0d2dc0f7dc10dbb81646b2914be26dbbb1da3ed7
JoelBaig <131587984+Joe482@users.noreply.github.com>
2025-02-26 20:49:20 +0100

    edited takeCard, shows random card now by picking up from stack

--- ringoffire/src/app/game/game.component.ts
+++ ringoffire/src/app/game/game.component.ts
@@ -1,16 +1,40 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Game } from '../../models/game';
+import { NgIf, NgFor, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgIf, NgFor, NgStyle],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
+  pickCardAnimation = false;
+  currentCard: string = '';
+  game: Game = new Game;
+
   @Input() card = {
     image: './assets/img/cards/card_cover.png',
     name: 'Card Name'
+  }
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
+  takeCard() {
+    if (this.game.stack.length > 0) {
+      this.currentCard = this.game.stack.pop() ?? 'card_cover';
+      this.pickCardAnimation = true;
+    } else {
+      console.warn("Keine Karten mehr im Stapel!");
+    }
   }
 }
