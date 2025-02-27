da81fd049bcfb3cfcf8969e832f1c328120a3db6
JoelBaig <131587984+Joe482@users.noreply.github.com>
2025-02-27 22:13:37 +0100

    fixed card animation after drawing

--- ringoffire/src/app/game/game.component.ts
+++ ringoffire/src/app/game/game.component.ts
@@ -32,12 +32,14 @@ export class GameComponent implements OnInit {
   takeCard() {
     if (this.game.stack.length > 0 && !this.pickCardAnimation) {
       this.currentCard = this.game.stack.pop() ?? 'card_cover';
-      console.log(this.currentCard);
       this.pickCardAnimation = true;
+      console.log('New Card: ' + this.currentCard);
+      console.log('Game is', this.game);
 
       setTimeout(() => {
+        this.game.playedCards.push(this.currentCard);
         this.pickCardAnimation = false;
-      }, 1500);
+      }, 1000);
     }
   }
 }
